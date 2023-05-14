ce3b821ad6010c003f0ded3388f90737b1a0dd8b
Mykhailo Sambur <m.sambur21@gmail.com>
2023-05-14 18:08:25 +0300

    hw5 update. getter setter added

--- src/hw5/Employee.java
+++ src/hw5/Employee.java
@@ -15,5 +15,43 @@ public class Employee {
         this.age = age;
     }
 
+    public String getFullName() {
+        return fullName;
+    }
 
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
