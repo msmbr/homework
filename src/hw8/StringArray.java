1c719ab8e2255d729db8007a81c19748acf76ea8
Mykhailo Sambur <m.sambur21@gmail.com>
2023-05-20 12:22:57 +0300

    hw8. get(index) check, delete(String v) update

--- src/hw8/StringArray.java
+++ src/hw8/StringArray.java
@@ -43,16 +43,16 @@ public class StringArray {
     }
 
     public boolean delete(String value) {
-        boolean result = false;
         for (int i = 0; i < arr.length; ++i) {
             if(arr[i].equals(value)) {
-                result = delete(i);
+                return delete(i);
             }
         }
-        return result;
+        return false;
     }
 
     public String get(int index) {
+        if (index >= arr.length || index<0) return "-1";
         return arr[index];
     }
 
